c88a8148876ec60f8631205ccb17ce35c66718c9
Ömer Faruk Kocadayı <omerkocadayi@gmail.com>
2017-04-02 01:05:02 +0300

    Update 10-girilen-sayiyi-ikilik-tabanda-yazma.py

--- 10-girilen-sayiyi-ikilik-tabanda-yazma.py
+++ 10-girilen-sayiyi-ikilik-tabanda-yazma.py
@@ -1,15 +1,13 @@
 def convert_binary(sayi):
     liste = []
-    i = 0
 
     while True:
-        liste.insert(i,sayi%2)
+        liste.insert(0,sayi%2)
         sayi = sayi // 2
 
         if sayi<2:
             liste.insert(0,sayi)
             break
-        i += 1
 
     print(liste)
 
